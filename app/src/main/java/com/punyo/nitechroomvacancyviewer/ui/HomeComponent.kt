3aa3482adedc992af8959f11c9e21b5efe2f7ef7
Punyo <49717092+Punyo@users.noreply.github.com>
2024-08-27 17:02:17 +0900

    Initial commit

---
+++ app/src/main/java/com/punyo/nitechroomvacancyviewer/ui/HomeComponent.kt
@@ -0,0 +1,10 @@
+package com.punyo.nitechroomvacancyviewer.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeComponent(modifier: Modifier = Modifier) {
+    Text(modifier = modifier, text = "TODO: HomeComponent")
+}
---
+++ app/src/main/java/com/punyo/nitechroomvacancyviewer/ui/SettingsComponent.kt
@@ -0,0 +1,11 @@
+package com.punyo.nitechroomvacancyviewer.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SettingsComponent(modifier: Modifier = Modifier)
+{
+    Text(modifier = modifier,text = "TODO: SettingsComponent")
+}
