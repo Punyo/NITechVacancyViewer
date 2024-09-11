83314ab53a7b42bda2f133b67278ce1f48f06710
Punyo <49717092+Punyo@users.noreply.github.com>
2024-09-12 01:23:11 +0900

    chore:不要なクラスを削除する

--- app/src/main/java/com/punyo/nitechroomvacancyviewer/data/room/RoomRepository.kt
+++ app/src/main/java/com/punyo/nitechroomvacancyviewer/data/room/RoomRepository.kt
@@ -1,24 +1,4 @@
 package com.punyo.nitechroomvacancyviewer.data.room
 
-import android.app.Application
-import android.util.Log
-import com.punyo.nitechroomvacancyviewer.data.auth.AuthRepository
-import com.punyo.nitechroomvacancyviewer.data.room.source.RoomNetworkDataSource
-
 object RoomRepository {
-    private val principalNameAndDisplayName = mutableMapOf<String, String>()
-
-    suspend fun getDisplayName(principalName: String): String {
-        if (principalNameAndDisplayName.contains(principalName)) {
-            return principalNameAndDisplayName[principalName]!!
-        } else {
-
-        }
-        return ""
-    }
-
-    suspend fun login(application: Application) {
-        RoomNetworkDataSource.login(application, AuthRepository.currentToken!!)
-            ?.let { Log.d("RoomRepository", it) }
-    }
 }
