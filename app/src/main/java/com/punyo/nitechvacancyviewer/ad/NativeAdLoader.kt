b6a857bdf016b1f438ba563ce4e46faab770ee28
Punyo <49717092+Punyo@users.noreply.github.com>
2024-10-02 17:45:20 +0900

    feat:NativeAdのキャッシュ機能を実装する

--- app/src/main/java/com/punyo/nitechvacancyviewer/ad/NativeAdLoader.kt
+++ app/src/main/java/com/punyo/nitechvacancyviewer/ad/NativeAdLoader.kt
@@ -4,46 +4,84 @@ import android.content.Context
 import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.nativead.NativeAd
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
 import java.util.Date
 import kotlin.coroutines.resume
 
 object NativeAdLoader {
-    private val loadedAds: MutableMap<Long, NativeAd> = mutableMapOf()
+    private val cachedAds: MutableList<NativeAdWithConsumeTime> = mutableListOf()
+    private val currentAds: MutableMap<String, NativeAdWithConsumeTime> = mutableMapOf()
 
-    private suspend fun loadAd(context: Context): Long {
+    private suspend fun loadAd(
+        context: Context,
+        adUnitId: String,
+        amount: Int
+    ): List<NativeAdWithConsumeTime> {
         return suspendCancellableCoroutine { continuation ->
-            val adLoader = AdLoader.Builder(context, AdConstants.NATIVE_VACANCYCOMPONENT_AD)
+            var loadedAdNum = 0
+            val loadedAds = mutableListOf<NativeAdWithConsumeTime>()
+            val adLoader = AdLoader.Builder(context, adUnitId)
                 .forNativeAd { nativeAd ->
                     if (continuation.isActive) {
                         val time = Date().time
-                        loadedAds[Date().time] = nativeAd
-                        continuation.resume(time)
+                        loadedAdNum++
+                        loadedAds.add(NativeAdWithConsumeTime(nativeAd, time))
+                        if (loadedAdNum == amount) {
+                            continuation.resume(loadedAds)
+                        }
                     }
                 }
                 .build()
-            adLoader.loadAd(AdRequest.Builder().build())
+            adLoader.loadAds(AdRequest.Builder().build(), amount)
         }
     }
 
-    suspend fun getAd(context: Context): NativeAd {
-        return if (loadedAds.isEmpty()) {
-            loadedAds[loadAd(context)]!!
-        } else {
-            val lastLoadTime = loadedAds.keys.first()
-            if (Date().time - lastLoadTime > AdConstants.NATIVE_AD_REFRESH_INTERVAL_MILLISECOND) {
-                refreshAd(context)
+    fun loadAdAndCache(context: Context, amount: Int, adUnitId: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            loadAd(context, adUnitId, amount).forEach(cachedAds::add)
+        }
+    }
+
+    suspend fun getAd(context: Context, adUnitId: String): NativeAd {
+        val currentAd = currentAds[adUnitId]
+        if (currentAd != null) {
+            return if (currentAd.consumeTime!! + AdConstants.NATIVE_AD_REFRESH_INTERVAL_MILLISECOND < Date().time) {
+                refreshAd(context, adUnitId)
             } else {
-                loadedAds[lastLoadTime]!!
+                currentAd.nativeAd
+            }
+        } else {
+            val cachedAd = cachedAds.removeFirstOrNull()
+            if (cachedAd != null) {
+                currentAds[adUnitId] = cachedAd.copy(consumeTime = Date().time)
+                return cachedAd.nativeAd
+            } else {
+                loadAd(context, adUnitId, 1).firstOrNull()?.let {
+                    currentAds[adUnitId] = it.copy(consumeTime = Date().time)
+                    return it.nativeAd
+                }
             }
         }
+        return loadAd(context, adUnitId, 1).first().nativeAd
     }
 
-    private suspend fun refreshAd(context: Context): NativeAd {
-        val lastLoadTime = loadedAds.keys.first()
-        val oldAd = loadedAds.remove(lastLoadTime)
-        oldAd!!.destroy()
-        return loadedAds[loadAd(context)]!!
+    private suspend fun refreshAd(context: Context, adUnitId: String): NativeAd {
+        val currentAd = currentAds[adUnitId]
+        if (currentAd != null) {
+            currentAds.remove(adUnitId)!!.nativeAd.destroy()
+            val newAd = loadAd(context, adUnitId, 1).first()
+            currentAds[adUnitId] = newAd.copy(consumeTime = Date().time)
+            return currentAds[adUnitId]!!.nativeAd
+        } else {
+            throw NullPointerException("No ad to refresh")
+        }
     }
-
 }
+
+private data class NativeAdWithConsumeTime(
+    val nativeAd: NativeAd,
+    val consumeTime: Long? = null
+)
