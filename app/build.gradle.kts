c8cbf7daf645bc53cf74dab4395ca6b499c9330b
Punyo <49717092+Punyo@users.noreply.github.com>
2024-12-13 11:56:11 +0900

    fix:データ取得時にキーストアの読み込みに失敗したときにクラッシュする問題を修正する

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.punyo.nitechvacancyviewer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
