99521d26c67005ea1f4d7a233e5908b1c17ea48b
Punyo <49717092+Punyo@users.noreply.github.com>
2026-03-23 00:39:47 +0900

    chore: update version code and version name to 13 and 1.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.punyo.nitechvacancyviewer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.6.1"
+        versionCode = 13
+        versionName = "1.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
