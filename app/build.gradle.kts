ff604530380913b33460b7200e95dcb25ecf6a81
Punyo <49717092+Punyo@users.noreply.github.com>
2026-03-22 18:20:41 +0900

    fix: バージョンコードとバージョン名を更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.punyo.nitechvacancyviewer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.6"
+        versionCode = 12
+        versionName = "1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
